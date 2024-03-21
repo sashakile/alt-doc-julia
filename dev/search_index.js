6b3aad1e34515c4db3d18fba2d7dc295933efc62
Documenter.jl <documenter@juliadocs.github.io>
2024-03-21 23:02:28 +0000

    build based on 4b499ad

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"documenter/#Tips-de-Documenter","page":"Tips de Documenter","title":"Tips de Documenter","text":"","category":"section"},{"location":"#alt-doc.jl","page":"Home","title":"alt-doc.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for alt-doc.jl","category":"page"}]
+}
